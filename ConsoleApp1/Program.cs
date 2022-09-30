640d6b7ee03516db5f923ffd4c1c65980f049426
aars4 <aars4@DESKTOP-EFN577A>
2022-09-30 20:35:26 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿//22 ИСП 2/2 Чистков Арсений
+
+Console.WriteLine("Введите x:");
+var x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+var y = double.Parse(Console.ReadLine());
+var result = Math.Pow(1 - Math.Tan(x), 1 / Math.Tan(x)) + Math.Cos(x - y);
+Console.WriteLine($"(1 - tgx)^ctgx+cos(x-y)={result}");
+Console.ReadLine();
